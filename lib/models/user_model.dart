49aef71176294545019e2c5fe15d2b23343c9468
hakim rebbouh <mkrym0955@gmail.com>
2026-08-05 22:18:09 +0100

    update the user model to match the one in backend

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -4,6 +4,10 @@ class UserModel {
   final String username;
   final String email;
   final bool emailVerified;
+  final String? phoneNumber;
+  final String? avatar;
+  final String? bio;
+  final DateTime? lastSeenAt;
 
   UserModel({
     required this.id,
@@ -11,6 +15,10 @@ class UserModel {
     required this.username,
     required this.email,
     required this.emailVerified,
+    this.phoneNumber,
+    this.avatar,
+    this.bio,
+    this.lastSeenAt,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
@@ -20,6 +28,12 @@ class UserModel {
       username: json['username'] as String,
       email: json['email'] as String,
       emailVerified: json['email_verified'] as bool? ?? false,
+      phoneNumber: json['phone_number'] as String?,
+      avatar: json['avatar'] as String?,
+      bio: json['bio'] as String?,
+      lastSeenAt: json['last_seen_at'] != null
+          ? DateTime.tryParse(json['last_seen_at'] as String)
+          : null,
     );
   }
 }
