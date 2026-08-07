34371dc1d07738ec20f31ab3d6678eff47c208a6
hakim rebbouh <mkrym0955@gmail.com>
2026-08-07 23:06:34 +0100

    add the new api's to constant.dart

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.100.101/api';
+  static const String baseUrl = 'http://10.0.2.2:8000/api';
 
   static const String register = '/auth/register';
   static const String login = '/auth/login';
@@ -7,6 +7,14 @@ class ApiConstants {
   static const String logout = '/auth/logout';
   static const String me = '/auth/me';
   static const String profile = '/profile';
+
+  static const String conversations = '/conversations';
+
+  static String conversationDetail(int id) => '/conversations/$id';
+  static String conversationMessages(int id) => '/conversations/$id/messages';
+  static String conversationRead(int id) => '/conversations/$id/read';
+  static String messageDetail(int conversationId, int messageId) =>
+      '/conversations/$conversationId/messages/$messageId';
 
   static const String googleServerClientId =
       'YOUR_GOOGLE_WEB_CLIENT_ID.apps.googleusercontent.com';
